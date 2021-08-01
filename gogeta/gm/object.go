247c31f1ae3bfb28305562cb6ac3867bf7ef0383
pikomonde <32364823+pikomonde@users.noreply.github.com>
2021-08-01 18:04:44 +0700

    prepare for removing behaviourObject

--- gogeta/gm/object.go
+++ gogeta/gm/object.go
@@ -1,6 +1,7 @@
 package gm
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -15,32 +16,62 @@ type objectData struct {
 	// behaviours behaviours
 }
 
+// Example 1: all object pointer & finding by exact
+//            object pointer								map[obj]				map["*objPointer"]objectData
+//
+// Example 2: finding by object type						map[objT::"objType"]	map["*objPointer"]objectData
 // objects stored objectData with mapping by objectType and objectName.
 // For example. there are 2 "enemy" objects and 1 "player" object, first map
 // indexed by "enemy" and "player", meanwhile the second mapping contain the
 // address (interface) of "enemy 1", "enemy 2", and "player 1".
+//
+// Example 3: finding by exact behaviour pointer (for
+//            parent object case)							map[bhvr::"*bhvPointer"]map["*objPointer"]objectData
+//
+// Example 4: finding by z-index							map[zidx]				map["*objPointer"]objectData
 type objects map[string]map[Object]objectData
 
-// TODO: #redesignmapobject
-// Example 1: all object pointer							map[obj][]objectData
-// Example 2: finding by exact object pointer				map[obj::"*objPointer"][0]objectData
-// Example 3: finding by object type						map[objT::"objType"][]objectData
-// Example 4: finding by exact behaviour pointer (for
-//            parent object case)							map[bhv::"*bhvPointer"][0]objectData
-// Example 5: finding by z-index							map[zidx][]objectData
-// type objects map[string][]objectData
+const (
+	keyByObj     = "obj"
+	keyByObjType = "objT::"
+	keyByBhvr    = "bhvr::"
+	keyByZIdx    = "zidx"
+)
 
-// Set an Instance to Game. It groups Instances based on Object type. What is
-// actually stored is the pointer (interface) of the instance.
+// Set an Instance to Game.
 func (objs objects) setObject(obj Object) {
-	objType := reflect.TypeOf(obj).String()
-	if _, ok := objs[objType]; !ok {
-		objs[objType] = make(map[Object]objectData)
-	}
-	objs[objType][obj] = objectData{
+	objd := objectData{
 		object: obj,
 		// behaviours: make(behaviours),
 	}
+	objType := reflect.TypeOf(obj).String()
+
+	// case: keyByObj
+	key := keyByObj
+	if _, ok := objs[key]; !ok {
+		objs[key] = make(map[Object]objectData)
+	}
+	objs[key][obj] = objd
+
+	// case: keyByObjType
+	key = fmt.Sprintf("%s%s\n", keyByObjType, objType)
+	if _, ok := objs[key]; !ok {
+		objs[key] = make(map[Object]objectData)
+	}
+	objs[key][obj] = objd
+}
+
+// Delete an Instance from Game.
+func (objs objects) delObject(obj Object) {
+	objType := reflect.TypeOf(obj).String()
+
+	// case: keyByObj
+	key := keyByObj
+	delete(objs[key], obj)
+
+	// case: keyByObjType
+	key = fmt.Sprintf("%s%s\n", keyByObjType, objType)
+	delete(objs[key], obj)
 }
 
 func (objs objects) getObjectData(obj Object) objectData {
@@ -50,6 +81,7 @@ func (objs objects) getObjectData(obj Object) objectData {
 
 // Update all Instances.
 func (objs objects) Update() {
+	// TODO: behaviours should be updated outside from this loop
 	for _, val := range objs {
 		for _, objData := range val {
 			updateBehaviours(objData.object)
@@ -60,6 +92,8 @@ func (objs objects) Update() {
 
 // Draw all Instances.
 func (objs objects) Draw(screen Screen) {
+	// TODO: consider z-index when draw objects
+	// TODO: behaviours should be draw outside from this loop
 	for _, val := range objs {
 		for _, objData := range val {
 			drawBehaviours(objData.object, screen)
